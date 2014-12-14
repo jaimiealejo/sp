e3c5d97305e42baa50469510b725eb9ea24560fc
Jaimielyn W. Alejo <jaimie.alejo06@gmail.com>
2014-12-14 16:54:18 +0800

    user access
    Signed-off-by: Jaimielyn W. Alejo <jaimie.alejo06@gmail.com>

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :capital_price, :info, :name, :selling_price, :prod_type
   validates :name, presence: true
-  validates :capital_price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than: 0}
+  validates :capital_price, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than: 0}
   validates :selling_price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than: 0}
   def stock_available
     Inventory.where(product_id:self.id).sum(:quantity)
