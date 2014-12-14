b4731ca7e257253ff436efb4fbfa3f10b6c5ea26
erikathea <ethajes@gmail.com>
2014-12-14 14:32:31 +0800

    add validation for invoice detail quantity and stock

--- app/models/invoice_detail.rb
+++ app/models/invoice_detail.rb
@@ -1,8 +1,19 @@
 class InvoiceDetail < ActiveRecord::Base
   belongs_to :type
   attr_accessible :price, :quantity, :invoice_type, :product_id, :procedure_id
+  validate :stock_quantity
   before_save :compute_price
   after_create :update_inventory
+
+  def stock_quantity
+    if self.product_id
+      product = Product.find(self.product_id)
+      stock = product.stock_available
+      if (stock < self.quantity)
+        errors.add(:quantity, "Only #{stock} left")
+      end
+    end
+  end
 
   def compute_price
   	if self.product_id
