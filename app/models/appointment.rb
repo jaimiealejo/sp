ee427c013c01d6db32a9e0615c8db9c5addb222f
Jaimielyn W. Alejo <jaimie.alejo06@gmail.com>
2015-01-20 17:28:29 +0800

    added if appointment.id == self.id inside for loop
    Signed-off-by: Jaimielyn W. Alejo <jaimie.alejo06@gmail.com>

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -19,18 +19,19 @@ class Appointment < ActiveRecord::Base
   	q = "%#{self.sched.split(' ').first}%"
   	appointments = Appointment.where("sched like ?", q)
   	appointments.each do |appointment|
-      if self.sched == appointment.sched
-        return errors.add(:sched, "Schedule is already taken. Please select another date/time.")
-      end
-
-      to_range = DateTime.parse(appointment.sched)
-  	  if appointment.est_time.present? && !appointment.est_time.to_i.zero?
-		    end_range = to_range + appointment.est_time.to_i.minutes
-        date_range = to_range.strftime('%H%M').to_i...end_range.strftime('%H%M').to_i
-  	    if date_range.include?DateTime.parse(self.sched).strftime('%H%M').to_i
+      next if appointment.id == self.id
+        if self.sched == appointment.sched
           return errors.add(:sched, "Schedule is already taken. Please select another date/time.")
         end
+        
+        to_range = DateTime.parse(appointment.sched)
+  	    if appointment.est_time.present? && !appointment.est_time.to_i.zero?
+		      end_range = to_range + appointment.est_time.to_i.minutes
+          date_range = to_range.strftime('%H%M').to_i...end_range.strftime('%H%M').to_i
+  	      if date_range.include?DateTime.parse(self.sched).strftime('%H%M').to_i
+            return errors.add(:sched, "Schedule is already taken. Please select another date/time.")
+          end
+  	    end
   	  end
-  	end
+    end
   end
-end
